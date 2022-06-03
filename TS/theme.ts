f5669c45d1a2efd804bb4964549a834c1ceaa21e
ApiProgrammer <ApiProgrammer@tutanota.com>
2022-06-02 23:02:21 -0300

    camelo 1.1

---
+++ TS/theme.ts
@@ -0,0 +1,26 @@
+switch (localStorage.getItem('DarkTheme')) {
+    case 'true':
+        document.getElementById('theme').setAttribute('href', '/CSS/dark.css')
+        break
+    case 'false':
+        document.getElementById('theme').setAttribute('href', '/CSS/light.css')
+        break
+    default:
+        localStorage.setItem('DarkTheme', matchMedia('(prefers-color-scheme: dark)')
+                    .matches
+                    .toString())
+        if (localStorage.getItem('DarkTheme') == 'true') 
+            document.getElementById('theme').setAttribute('href', '/CSS/dark.css')
+        else
+            document.getElementById('theme').setAttribute('href', '/CSS/light.css')
+}
+
+const invertTheme = () => {
+    switch (localStorage.getItem('DarkTheme')) {
+        case 'true':
+            localStorage.setItem('DarkTheme', 'false')
+            break
+        default:
+            localStorage.setItem('DarkTheme', 'true')
+    }
+}
